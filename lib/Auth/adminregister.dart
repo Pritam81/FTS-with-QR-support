0592e55776b45e47c4fd9f440909ca6e3fad9741
Pritam STP <93328629+Pritam81@users.noreply.github.com>
2025-06-14 20:51:51 +0530

    login and register file created

---
+++ lib/Auth/adminregister.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminRegister extends StatefulWidget {
+  const AdminRegister({super.key});
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/Auth/employeelogin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Employeelogin extends StatefulWidget {
+  const Employeelogin({super.key});
+
+  @override
+  State<Employeelogin> createState() => _EmployeeloginState();
+}
+
+class _EmployeeloginState extends State<Employeelogin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
